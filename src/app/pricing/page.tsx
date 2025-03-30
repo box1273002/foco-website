e03f9e7e1e0d8af2b713674f6ffc07d8b1b488d1
Ptolemy Morris <ptolemymorris@gmail.com>
2025-03-30 00:15:44 +0000

    Added pricing page still wiggles

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -2,14 +2,14 @@
 import { Container } from "@/components/Container";
 import { PricingBox } from "@/components/pricing";
 export default function Pricing() {
+  const free = ["iOS & Andriod"]
+  const premium = ["Take back control", "More flexiblity"]
   return (
-    <Container>
-      <div className="flex flex-wrap justify-center">
-        <PricingBox />
-        <PricingBox />
-        <PricingBox />
-      </div>
+    <div className="flex flex-wrap md:flex-nowrap justify-center w-full">
+      <PricingBox title="Free" month={0} year={0} lifetime={0} benefits={free} />
+      <PricingBox title="Premium" month={1} year={10} lifetime={25} benefits={premium} />
+      <PricingBox title="Student" month={0.79} year={8} lifetime={20} benefits={premium} />
+    </div>
 
-    </Container>
   )
 }
