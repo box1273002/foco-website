418d27cf4c694fa4b487b3b71384c08aa741a9af
Ptolemy Morris <ptolemymorris@gmail.com>
2025-04-12 19:06:43 +0100

    Change coming soon video

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -5,7 +5,7 @@ export default function Pricing() {
   return (
     <Container>
       <div className="flex flex-wrap justify-center">
-        <Video videoId="U8v16kFEt-o" />
+        <Video videoId="JrM_rucl2pE" />
       </div>
 
     </Container>
